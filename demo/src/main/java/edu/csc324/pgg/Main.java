742dc03ed0e89ab638f22375c080c8656a6e4d95
Yash Malik Lenovo Portable <yash.malik0606@gmail.com>
2025-11-09 15:17:12 -0600

    maven architecture

---
+++ demo/src/main/java/edu/csc324/pgg/Main.java
@@ -0,0 +1,7 @@
+package edu.csc324.pgg;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/README.md
+++
@@ -1,1 +0,0 @@
-This Project is being created by Adarsh, Chloe, Siho and Yash. 
